f7e7753d3021fdfddce1b30be8902531af89d11e
Marius Mailat <marius.mailat@appsrise.com>
2016-01-25 15:38:18 +0300

    Homework and homelab.

--- yamba-test/app/src/main/java/com/example/android/yamba/BootReceiver.java
+++ yamba-test/app/src/main/java/com/example/android/yamba/BootReceiver.java
@@ -10,6 +10,7 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
+
     private static final String TAG = BootReceiver.class.getSimpleName();
     private static final long DEFAULT_INTERVAL = 0;
 
