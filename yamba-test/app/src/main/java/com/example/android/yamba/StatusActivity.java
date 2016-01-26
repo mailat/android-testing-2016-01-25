d2a09002905f930079c6f0023cb949ff062f7a38
Marius Mailat <marius.mailat@appsrise.com>
2016-01-26 15:57:48 +0300

    Homework for Day 2 plus finished homelab.

--- yamba-test/app/src/main/java/com/example/android/yamba/StatusActivity.java
+++ yamba-test/app/src/main/java/com/example/android/yamba/StatusActivity.java
@@ -6,13 +6,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class StatusActivity extends AppCompatActivity implements
